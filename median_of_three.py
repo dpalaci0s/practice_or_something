0f90345f8052e899b83272aeaa84e6c0dc4ee053
dpalaci0s <dpalaci0s@users.noreply.github.com>
2014-12-02 11:32:36 -0600

    Update median_of_three.py
    removed cheap shit and I think I removed a line or something

--- median_of_three.py
+++ median_of_three.py
@@ -1,4 +1,4 @@
+import math
 for i in xrange(0,int(raw_input())):
     nums = map(int,raw_input().split())
-    list.sort(nums)
-    print nums[1],
+    print (nums[0]+nums[1]+nums[2])-min(nums[0],nums[1],nums[2])-max(nums[0],nums[1],nums[2])
